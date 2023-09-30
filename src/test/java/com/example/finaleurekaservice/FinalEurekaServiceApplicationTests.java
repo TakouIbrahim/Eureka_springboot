2d99221e1b5a3ba3ec752ecae5339fba06afc75a
TakouIbrahim <nonomoyenn@gmail.com>
2023-09-30 18:07:24 +0200

    first commit

---
+++ src/test/java/com/example/finaleurekaservice/FinalEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finaleurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
